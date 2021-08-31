31475bcdba1e114fa2ed851fe60fa7ddc8bb8b1c
hxxdini <blackhxxdini@gmail.com>
2021-08-31 16:55:09 +0100

    Backend setup

---
+++ pages/api/apex/index.js
@@ -0,0 +1,22 @@
+import axios from "axios"
+
+export default async function handler(req, res) {
+
+    // Get this data from frontend
+    const { platform, platformUserIdentifier } = req.body
+
+    const apiUrl = "https://public-api.tracker.gg/v2/apex/standard/profile/" + platform + "/" + platformUserIdentifier
+
+    try {
+        const data = await axios.get(apiUrl, { headers: { "TRN-Api-Key": process.env.APEX_TRN_API_KEY } })
+        // Print first the data to see what to capture 
+        console.log(data.data)
+        // Capture the end data content ie .. Find the property to return as response
+        // ex: data.data["profile"] if profile is found in the data log.
+        const userData = JSON.parse(JSON.stringify(data.data))
+        res.status(200).json(userData)
+    } catch (e) {
+        console.log(e.message)
+        res.status(200).end()
+    }
+}
