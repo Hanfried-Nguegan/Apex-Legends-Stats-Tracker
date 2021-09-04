242f7733b3d372512000153c27cfecec517a331f
Hanfried-Nguegan <80390250+Hanfried-Nguegan@users.noreply.github.com>
2021-09-04 20:57:14 +0530

    my new commit

--- components/Banner.js
+++ components/Banner.js
@@ -5,20 +5,20 @@ import ApexGif from "../public/images/apex_logo_gif.gif";
 import XboxLogo from "../public/images/xbox_logo.png";
 import OriginLogo from "../public/images/origin_logo.png";
 import PsLogo from "../public/images/ps4_logo.png";
-import Leader from "./Leader";
 import axios from "axios";
+import { useRouter } from "next/dist/client/router";
+import Alert from "./Alert";
 
 function Banner() {
-  const [platform, setPlatform] = useState("psn")
-
-  const [platformUserIdentifier, setPlatformUserIdentifier] = useState("")
-
-  const [userProfile, setUserProfile] = useState()
+  const [platform, setPlatform] = useState("psn");
+  const [platformUserIdentifier, setPlatformUserIdentifier] = useState("");
+  const [userProfile, setUserProfile] = useState();
+  const router = useRouter();
 
   // console.log(platform)
   // console.log(platformUserIdentifier)
 
-  const getUserProfile = async () => {
+  const getUserProfile = async (e) => {
     // Capture data for platform and platformUserId
     const data = await axios.post("/api/apex/", {
       platform,
@@ -26,13 +26,23 @@ function Banner() {
     });
     const profile = JSON.parse(JSON.stringify(data.data));
     // You have the user profile
-    console.log(profile)
-    setUserProfile(profile)
+    console.log(profile);
+    setUserProfile(profile);
+
+    if (userProfile) {
+      router.push({
+        pathname: "/search",
+        query: {
+          userName: platformUserIdentifier,
+          platform: platform,
+        },
+      });
+    }
   };
 
   return (
     <div className="bg-cover h-screen bg-worlds-edge">
-      <div className="flex flex-col lg:flex-row lg:pt-16 lg:pr-16 lg:pb-32 lg:pl-16 items-center justify-between pt-1 pr-1 pb-8 pl-1">
+      <div className="flex flex-col lg:flex-row lg:pt-16 lg:pr-16 lg:pb-32 lg:pl-16 items-center h-screen justify-center pt-1 pr-1 pb-8 pl-1">
         <div className="flex flex-col w-full md:w-11/12 justify-between items-center ">
           <div className="flex items-center space-x-3 mt-1 mb-2 ml-0 mr-0">
             <div className="mr-1">
@@ -50,7 +60,7 @@ function Banner() {
           <div className="flex mb-2 w-full lg:w-3/5 md:w-4/5 flex-col ">
             <div className="flex justify-between p-2  m-2 h-16 bg-gray-900 rounded-md bg-opacity-50">
               <div
-                className="flex flex-grow justify-center cursor-pointer"
+                className="flex flex-grow justify-center cursor-pointer "
                 onClick={(e) => setPlatform("origin")}
               >
                 <Image
@@ -84,43 +94,24 @@ function Banner() {
                 />
               </div>
             </div>
-            <div className="flex items-center">
+            <div className="flex relative items-center">
               <input
                 value={platformUserIdentifier}
                 onChange={(e) => setPlatformUserIdentifier(e.target.value)}
                 type="text"
-                placeholder="Enter A Username"
+                placeholder={
+                  (platform == "psn" && "Enter PlayStation User Name") ||
+                  (platform == "xbl" && "Enter XBox Live User Name") ||
+                  (platform == "origin" && "Enter Origin User Name")
+                }
                 className="flex flex-1 m-2 p-2 h-16 rounded-md outline-none"
               />
-              <SearchIcon className="bg-white" onClick={getUserProfile} />
+              <SearchIcon
+                className=" absolute right-0 mr-4 cursor-pointer"
+                onClick={getUserProfile}
+              />
             </div>
           </div>
-          {/* Display data */}
-          <div className="flex items-center">
-            {userProfile && userProfile.metadata.activeLegendName}
-          </div>
-        </div>
-        <div className="flex flex-col py-5 w-[400px] md:w-[500px] lg:w-[600px] justify-center mt-2 mr-4 ">
-          {/* Leader Component */}
-          <Leader
-            img={
-              "https://avatar-cdn.tracker.gg/api/avatar/2/youaintgangggang.png"
-            }
-            name={"youaintgangggang"}
-            kills={"10058777"}
-          />
-          <Leader
-            img={
-              "https://imgsvc.trackercdn.com/url/size(64),fit(cover)/https%3A%2F%2Favatar-cdn.tracker.gg%2Fapi%2Favatar%2F2%2Fema.png/image.png"
-            }
-            name={"noobkiller"}
-            damage={"58884022"}
-          />
-          <Leader
-            img={"https://avatars.githubusercontent.com/u/42778495?v=4"}
-            name={"hxxdini"}
-            wins={"10586"}
-          />
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
-import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -9,17 +8,10 @@ export default function Home() {
         <title>Apex tracker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
+
       <main>
-        <section>
-          <Banner />
-        </section>
-        <section>
-          {/* Legend Meta */}
-          {/* LeaderBoard */}
-        </section>
+        <Banner />
       </main>
-      <footer>{/* Footer Component */}</footer>
     </div>
   );
 }
