0fee99dd19bfe0073b578a0827091501c69287d9
Hanfried-Nguegan <80390250+Hanfried-Nguegan@users.noreply.github.com>
2021-09-17 01:26:21 +0530

    new commit

--- components/player/LifetimeStats.js
+++ components/player/LifetimeStats.js
@@ -1,6 +1,6 @@
 function LifetimeStats({ title, value }) {
   return (
-    <div className="flex flex-col items-center bg-gunpowder h-14 lg:h-16 w-[70px] md:w-24 lg:w-36 rounded-sm p-2 border-t-8 border-peakypink ">
+    <div className="flex flex-col items-center bg-gunpowder h-14 lg:h-16 w-[5.5rem] md:w-24 lg:w-36 rounded-sm p-2 border-t-8 border-peakypink ">
       <p className="text-sm font-semibold text-white lg:text-lg lg:font-bold">
         {value}
       </p>
